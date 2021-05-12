d5ee4b15c4bb09971130469522bbaa0061f8c1ba
Caio marquezini <caionmarqz@gmail.com>
2021-05-12 14:50:09 -0300

    added h2 database

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -1,24 +1,26 @@
 package com.devsuperior.dscatalog.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.services.CategoryService;
 
 @RestController
+@CrossOrigin
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 
+	private CategoryService service;
+	
 	@GetMapping
 	public ResponseEntity<List<Category>> findall() {
-		List<Category> list = new ArrayList<>();
-		list.add(new Category(1L, "Books"));
-		list.add(new Category(2L, "Electronics"));
+		List<Category> list = service.findall();
 		return ResponseEntity.ok(list);
 	}
 	
