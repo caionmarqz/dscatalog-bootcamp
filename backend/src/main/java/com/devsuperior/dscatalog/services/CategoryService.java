c0a12d543a625c7ff32d5d3151e3ecff9de76d25
Caio marquezini <caionmarqz@gmail.com>
2021-05-13 09:02:20 -0300

    http not found (404) exception handler added

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,10 +1,16 @@
 package com.devsuperior.dscatalog.services;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repository.CategoryRepository;
 
@@ -14,9 +20,18 @@ public class CategoryService {
 	@Autowired
 	CategoryRepository categoryRepository;
 	
-	
-	public List<Category> findall() {
-		return categoryRepository.findAll();
+	@Transactional(readOnly = true)
+	public List<CategoryDTO> findall() {
+		List<Category> list = categoryRepository.findAll();
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+	}
+
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = categoryRepository.findById(id);
+		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		return new CategoryDTO(entity);
 	}
 	
 	
